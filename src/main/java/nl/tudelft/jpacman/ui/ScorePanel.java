2ea02e768596eb9417b532cf731f8368eef36da7
vbxd287 <vbao2814@gmail.com>
2025-07-23 20:46:37 +0700

    Update ScorePanel.java

--- src/main/java/nl/tudelft/jpacman/ui/ScorePanel.java
+++ src/main/java/nl/tudelft/jpacman/ui/ScorePanel.java
@@ -10,42 +10,22 @@ import javax.swing.JPanel;
 
 import nl.tudelft.jpacman.level.Player;
 
-/**
- * A panel consisting of a column for each player, with the numbered players on
- * top and their respective scores underneath.
- *
- * @author Jeroen Roosen 
- *
- */
+
 public class ScorePanel extends JPanel {
 
-    /**
-     * Default serialisation ID.
-     */
+
     private static final long serialVersionUID = 1L;
 
-    /**
-     * The map of players and the labels their scores are on.
-     */
     private final Map<Player, JLabel> scoreLabels;
 
-    /**
-     * The default way in which the score is shown.
-     */
+ 
     public static final ScoreFormatter DEFAULT_SCORE_FORMATTER =
         (Player player) -> String.format("Score: %3d", player.getScore());
 
-    /**
-     * The way to format the score information.
-     */
+    
     private ScoreFormatter scoreFormatter = DEFAULT_SCORE_FORMATTER;
 
-    /**
-     * Creates a new score panel with a column for each player.
-     *
-     * @param players
-     *            The players to display the scores of.
-     */
+  
     public ScorePanel(List<Player> players) {
         super();
         assert players != null;
@@ -63,9 +43,7 @@ public class ScorePanel extends JPanel {
         }
     }
 
-    /**
-     * Refreshes the scores of the players.
-     */
+   
     protected void refresh() {
         for (Map.Entry<Player, JLabel> entry : scoreLabels.entrySet()) {
             Player player = entry.getKey();
@@ -78,23 +56,14 @@ public class ScorePanel extends JPanel {
         }
     }
 
-    /**
-     * Provide means to format the score for a given player.
-     */
+ 
     public interface ScoreFormatter {
 
-        /**
-         * Format the score of a given player.
-         * @param player The player and its score
-         * @return Formatted score.
-         */
+     
         String format(Player player);
     }
 
-    /**
-     * Let the score panel use a dedicated score formatter.
-     * @param scoreFormatter Score formatter to be used.
-     */
+
     public void setScoreFormatter(ScoreFormatter scoreFormatter) {
         assert scoreFormatter != null;
         this.scoreFormatter = scoreFormatter;
