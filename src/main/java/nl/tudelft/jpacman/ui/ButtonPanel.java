556a5f1ff9a1275cf5e11e0f3a118e4ca62e59d9
vbxd287 <vbao2814@gmail.com>
2025-07-23 20:45:05 +0700

    Update ButtonPanel.java

--- src/main/java/nl/tudelft/jpacman/ui/ButtonPanel.java
+++ src/main/java/nl/tudelft/jpacman/ui/ButtonPanel.java
@@ -6,23 +6,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-/**
- * A panel containing a button for every registered action.
- *
- * @author Jeroen Roosen 
- */
+
 class ButtonPanel extends JPanel {
 
-    /**
-     * Default serialisation ID.
-     */
     private static final long serialVersionUID = 1L;
 
-    /**
-     * Create a new button panel with a button for every action.
-     * @param buttons The map of caption - action for each button.
-     * @param parent The parent frame, used to return window focus.
-     */
+
     ButtonPanel(final Map<String, Action> buttons, final JFrame parent) {
         super();
         assert buttons != null;
