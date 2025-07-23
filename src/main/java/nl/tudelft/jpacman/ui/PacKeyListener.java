0456def44b334163f8764ca12472779ec48e9f5a
vbxd287 <vbao2814@gmail.com>
2025-07-23 20:45:20 +0700

    Update PacKeyListener.java

--- src/main/java/nl/tudelft/jpacman/ui/PacKeyListener.java
+++ src/main/java/nl/tudelft/jpacman/ui/PacKeyListener.java
@@ -4,22 +4,13 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Map;
 
-/**
- * A key listener based on a set of keyCode-action pairs.
- *
- * @author Jeroen Roosen 
- */
+
 class PacKeyListener implements KeyListener {
 
-    /**
-     * The mappings of keyCode to action.
-     */
+ 
     private final Map<Integer, Action> mappings;
 
-    /**
-     * Create a new key listener based on a set of keyCode-action pairs.
-     * @param keyMappings The mappings of keyCode to action.
-     */
+  
     PacKeyListener(Map<Integer, Action> keyMappings) {
         assert keyMappings != null;
         this.mappings = keyMappings;
@@ -36,11 +27,11 @@ class PacKeyListener implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent event) {
-        // do nothing
+      
     }
 
     @Override
     public void keyReleased(KeyEvent event) {
-        // do nothing
+       
     }
 }
