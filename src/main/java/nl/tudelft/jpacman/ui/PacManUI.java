d0b3fc17cba55a5294bb595c466cbfcc53e419d3
vbxd287 <vbao2814@gmail.com>
2025-07-23 20:45:41 +0700

    Update PacManUI.java

--- src/main/java/nl/tudelft/jpacman/ui/PacManUI.java
+++ src/main/java/nl/tudelft/jpacman/ui/PacManUI.java
@@ -14,57 +14,19 @@ import javax.swing.WindowConstants;
 import nl.tudelft.jpacman.game.Game;
 import nl.tudelft.jpacman.ui.ScorePanel.ScoreFormatter;
 
-/**
- * The default JPacMan UI frame. The PacManUI consists of the following
- * elements:
- *
- * <ul>
- * <li>A score panel at the top, displaying the score of the player(s).
- * <li>A board panel, displaying the current level, i.e. the board and all units
- * on it.
- * <li>A button panel, containing all buttons provided upon creation.
- * </ul>
- *
- * @author Jeroen Roosen 
- *
- */
 public class PacManUI extends JFrame {
 
-    /**
-     * Default serialisation UID.
-     */
+  
     private static final long serialVersionUID = 1L;
 
-    /**
-     * The desired frame rate interval for the graphics in milliseconds, 40
-     * being 25 fps.
-     */
+
     private static final int FRAME_INTERVAL = 40;
 
-    /**
-     * The panel displaying the player scores.
-     */
     private final ScorePanel scorePanel;
 
-    /**
-     * The panel displaying the game.
-     */
+   
     private final BoardPanel boardPanel;
 
-    /**
-     * Creates a new UI for a JPac-Man game.
-     *
-     * @param game
-     *            The game to play.
-     * @param buttons
-     *            The map of caption-to-action entries that will appear as
-     *            buttons on the interface.
-     * @param keyMappings
-     *            The map of keyCode-to-action entries that will be added as key
-     *            listeners to the interface.
-     * @param scoreFormatter
-     *            The formatter used to display the current score.
-     */
     public PacManUI(final Game game, final Map<String, Action> buttons,
                     final Map<Integer, Action> keyMappings,
                     ScoreFormatter scoreFormatter) {
@@ -96,19 +58,13 @@ public class PacManUI extends JFrame {
         pack();
     }
 
-    /**
-     * Starts the "engine", the thread that redraws the interface at set
-     * intervals.
-     */
+   
     public void start() {
         setVisible(true);
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
         service.scheduleAtFixedRate(this::nextFrame, 0, FRAME_INTERVAL, TimeUnit.MILLISECONDS);
     }
 
-    /**
-     * Draws the next frame, i.e. refreshes the scores and game.
-     */
     private void nextFrame() {
         boardPanel.repaint();
         scorePanel.refresh();
